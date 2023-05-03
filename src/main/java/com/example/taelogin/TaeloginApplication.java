e7a0374a4d4e8c81a146fa70ac6bd58436124703
taeheoki <42.4.taeheoki@gmail.com>
2023-05-04 00:25:54 +0900

    feat: first commit

---
+++ src/main/java/com/example/taelogin/TaeloginApplication.java
@@ -0,0 +1,13 @@
+package com.example.taelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaeloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaeloginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taelogin/TaeloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taelogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaeloginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
